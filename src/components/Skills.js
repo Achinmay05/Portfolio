8df2a5d1888b664bc5a5eda09a046dca3058bb4f
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-08-22 23:13:45 +0530

    ...

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
-import TrackVisibility from "react-on-screen";
+// import TrackVisibility from "react-on-screen";
 
 export const Skills = () => {
     const responsive = {
@@ -41,25 +41,25 @@ export const Skills = () => {
 
                             <Carousel responsive={responsive} infinite={true} className="skill-slider">
                                 <div className="item">
-                                    <img src={meter1} alt="Image" />
+                                    <img src={meter1} alt="Skillmeter" />
                                     <h2>
                                         Web Development
                                     </h2>
                                 </div>
                                 <div className="item">
-                                    <img src={meter1} alt="Image" />
+                                    <img src={meter1} alt="Skillmeter" />
                                     <h2>
                                         C++
                                     </h2>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="Image" />
+                                    <img src={meter2} alt="Skillmeter" />
                                     <h2>
                                         Problem Solving
                                     </h2>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="Image" />
+                                    <img src={meter3} alt="Skillmeter" />
                                     <h2>
                                         Data Analytics
                                     </h2>
